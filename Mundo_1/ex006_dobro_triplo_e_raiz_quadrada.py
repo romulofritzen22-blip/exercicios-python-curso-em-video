309e8d59f81ff2773e791d931a1b574c018173bb
Romulo Fritzen <romulofritzen22@gmail.com>
2026-01-10 10:01:09 -0300

    melhora formatação e simplifica o cálculo de raiz

--- Mundo_1/ex006_dobro_triplo_e_raiz_quadrada.py
+++ Mundo_1/ex006_dobro_triplo_e_raiz_quadrada.py
@@ -4,4 +4,4 @@ n = int(input('Digite um número: '))
 
 print(f'O dobro de {n} é {n * 2}')
 print(f'O triplo de {n} é {n * 3}')
-print(f'A raíz quadrada de {n} é {round(n ** (1/2), 2)}')
+print(f'A raíz quadrada de {n} é {n ** 0.5:.2f}')
