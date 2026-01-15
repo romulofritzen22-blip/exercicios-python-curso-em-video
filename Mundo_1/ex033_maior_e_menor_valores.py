f8613ee771bda97df67c1c5b28f0cb2b0a7ccf37
Romulo Fritzen <romulofritzen22@gmail.com>
2026-01-15 16:48:47 -0300

    adiciona ex033

---
+++ Mundo_1/ex033_maior_e_menor_valores.py
@@ -0,0 +1,19 @@
+#Desafio 033: Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
+n1 = float(input('Digite o 1° número: '))
+n2 = float(input('Digite o 2° número: '))
+n3 = float(input('Digite o 3° número: '))
+
+maior = n1
+if n2 >= n1 and n2 >= n3:
+    maior = n2
+if n3 >= n1 and n3 >= n2:
+    maior = n3
+
+menor = n1
+if n2 <= n1 and n2 <= n3:
+    menor = n2
+if n3 <= n1 and n3 <= n2:
+    menor = n3
+
+print(f'O maior número é {maior} e o menor número é {menor}')
