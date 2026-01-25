0686ea45dce9275cfa1801988c730dc2edf4a5ef
Romulo Fritzen <romulofritzen22@gmail.com>
2026-01-25 18:06:55 -0300

    adiciona ex045

---
+++ Mundo_2/ex045_game_pedra_papel_e_tesoura.py
@@ -0,0 +1,21 @@
+from random import randint
+print(f'{' JOKENPÔ ':=^30}')
+usuario = int(input(f'1. Pedra \n2. Papel \n3. Tesoura \n{'=' * 30} \nOpção: '))
+pc = randint(1, 3)
+
+if usuario == pc:
+    print('EMPATE!')
+elif usuario == 1 and pc == 2:
+    print('Você PERDEU! O computador escolheu PAPEL!')
+elif usuario == 1 and pc == 3:
+    print('Você VENCEU! O computador escolheu TESOURA!')
+elif usuario == 2 and pc == 1:
+    print('Você VENCEU! O computador escolheu PEDRA!')
+elif usuario == 2 and pc == 3:
+    print('Você PERDEU! O computador escolheu TESOURA!')
+elif usuario == 3 and pc == 1:
+    print('Você PERDEU! O computador escolheu PEDRA!')
+elif usuario == 3 and pc == 2:
+    print('Você VENCEU! O computador escolheu PAPEL!')
+else:
+    print('ERRO! Escolha uma opção válida!')
